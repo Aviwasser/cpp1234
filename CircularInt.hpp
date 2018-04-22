9e1fdc3a581cbd1dc5a7d3365c19050778acf30b
Aviwasser <33319477+Aviwasser@users.noreply.github.com>
2018-04-23 01:40:58 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -19,23 +19,28 @@ public:
     /* Arithmetic operators */
     CircularInt& operator=(const int num);
     CircularInt& operator=(const CircularInt& c);
-
+    friend const CircularInt operator+(const int num , const CircularInt& c);
     friend const CircularInt operator+(const CircularInt& c , const int num);
     friend const CircularInt operator+(CircularInt const& c1 , CircularInt const& c2);
     friend const CircularInt operator-(const int num , CircularInt const& c);
-
+    friend const CircularInt operator-(const CircularInt& c , const int num);
     friend const CircularInt operator-(const CircularInt& c1 , const CircularInt& c2);
     CircularInt operator-();
     friend const CircularInt operator*(const CircularInt& c , const int num);
-
+    friend const CircularInt operator*(const int num , const CircularInt& c);
     friend const CircularInt operator/(CircularInt const& c , const int num);
     friend const CircularInt operator%(const CircularInt& c , const int num);
     CircularInt& operator++();
     CircularInt operator++(const int other);
     CircularInt& operator--();
     CircularInt operator--(const int other);
-
-
+    /* Comparison operators */
+    friend bool operator==(const CircularInt c1 , const CircularInt c2);
+    friend bool operator!=(const CircularInt c1 , const CircularInt c2); 
+    friend bool operator>(const CircularInt c1 , const CircularInt c2); 
+    friend bool operator<(const CircularInt c1 , const CircularInt c2);  
+    friend bool operator>=(const CircularInt c1 , const CircularInt c2); 
+    friend bool operator<=(const CircularInt c1 , const CircularInt c2); 
     /* Compound assignment operators */
     CircularInt& operator+=(const int other);
     CircularInt& operator+=(const CircularInt& other);
@@ -43,7 +48,9 @@ public:
     CircularInt& operator-=(const CircularInt& other);
     CircularInt& operator*=(const int other);
     CircularInt& operator*=(const CircularInt& other);  
-
+    CircularInt& operator/=(const int other);
+    CircularInt& operator%=(const int other);
+    /* IOstream */
     friend ostream& operator<<(std::ostream& os, CircularInt const& obj);
     friend istream& operator>>(istream& input, CircularInt& obj);
 };
